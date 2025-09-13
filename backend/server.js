42093bd0026cdf4bc245f86a39c6da93109ea91e
Reza Herdiasyah <rezaherdiansyah76@gmail.com>
2025-09-13 19:17:57 +0700

    update content

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,17 @@
-import express from "express";
-import cors from "cors";
-import contactRoutes from "./route/contact.js";
+// import express from "express";
+// import cors from "cors";
+// import contactRoutes from "./route/contact.js";
 
-const app = express();
-const PORT = 5500;
+// const app = express();
+// const PORT = 5500;
 
-// Middleware
-app.use(cors());
-app.use(express.json()); // biar bisa baca JSON
+// // Middleware
+// app.use(cors());
+// app.use(express.json()); // biar bisa baca JSON
 
-// Routes
-app.use("/api/contact", contactRoutes);
+// // Routes
+// app.use("/api/contact", contactRoutes);
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server running on http://localhost:${PORT}`);
+// });
